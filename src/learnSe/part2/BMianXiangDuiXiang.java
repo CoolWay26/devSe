17f29a6429c5bcbf297299707fa02e222e13958c
CoolWay26 <377905399@qq.com>
2019-08-29 21:42:30 +0800

    2.1面向对象（Ⅱ）

--- src/learnSe/part2/BMianXiangDuiXiang.java
+++ src/learnSe/part2/BMianXiangDuiXiang.java
@@ -71,15 +71,98 @@ package learnSe.part2;
 //        3.设计理念区别
 //            抽象类	    is	定义的是某些共性功能
 //            接口		have定义的是某些拓展功能
-
+//3.包package  其实就是文件夹
+//    1.注意
+//        1.package 包名;   包名一般为域名倒置（其余部分按功能和模块划分）
+//        2.package语句必须是程序的第一条可执行的代码，package语句在一个java文件中只能有一个，如果没有package，默认表示无包名
+//        3.命令行编译的时候，要使用javac -d xxx.java(会自动生成对应的包)     命令行运行要使用全类名 java x.x.x.x.xxx.java
+//    2.import关键字 import 全类名;
+//        1.作用：让有包的类对调用者可见,不再需要写全类名，而不是改变了访问权限
+//        2.注意：import时要写到类名，可以用*通配符表示整个包（尽量只导入需要的类）
+//    3.package,import,class的顺序关系
+//        package必须是可执行语句的第一行，且仅一句
+//        import放在中间，可以多句
+//        class在最后
+//4.四种权限修饰符
+//                   本类	    同一个包下(子类和无关类)   不同包下(子类)	不同包下(子类和无关类)
+//        private 	Y
+//        默认		Y		          Y
+//        protected	Y		          Y						Y
+//        public	    Y		          Y						Y				Y
+//    private:本类中可以调用（访问，使用）
+//    default:本包中可以调用（访问，使用）
+//    projected:
+//        本包中，不同包的子类中通过super或者（this）访问，其实super和this访问的是同一个对象
+//        要明确的是，实际上并不是直接访问父类，而是访问继承而来的成员（其实还是访问子类对象）
+//        并不能通过父类对象直接访问projected成员
+//            比如：子父类不在同一包下，
+//            package p1;
+//            class Fu {
+//                protected String name = "老父亲！";
+//            }
+//
+//            package p2;
+//            class Zi extends Fu{
+//                String name = this.name;    //获取了继承来的父类projected成员
+//                Zi zi = new Zi();
+//                String name1 = zi.name;     //子类访问继承的父类projected成员（其实相当于访问自己的）
+//                String name2 = super.name;   //super获取父类projected成员（其实也相当于访问自己的）
+//                Fu fu = new Fu();
+//                String name3 = fu.name;     //报错，因为不同包下子类其实并不能直接访问父类的projected成员（要么通过子类访问继承，要么super访问）
+//
+//            }
+//        重写的时候声明权限只能更大-public不能更低-private，最好一致
+//    public:完全公开访问权限
+//5.其他修饰符
+//    状态修饰符：static，final
+//    抽象修饰符：abstract
+//6.成员内部类（归根到底还是外部类的成员）
+//    1.特点
+//        1.内部类中可以直接访问外部类的成员，包括私有（因为内部类可以获得外部类的引用Outer.this）
+//        2.外部类要访问内部类的成员，必须创建对象：Outer.Inner oi = new Outer().new Inner();
+//        3.和其他的类一样，成员可以定义成private，提供public方法访问
+//        4.内部类本身访问权限不能超过外部类（亲测），这也很好理解，因为访问内部类的前提是访问外部类
+//    2.静态成员内部类   用static修饰
+//        外部类名.内部类名 对象名 = 外部类名.内部类对象; Outer.Inner oi = new Outer.Inner();相当于外部类名调用内部静态成员，习惯把new提前
+//    3.局部内部类（成员方法内的内部类）
+//        jdk8以前，局部内部类访问其所在方法的局部变量时，该变量必须是final修饰的自定义常量
+//            原因：
+//                1.生命周期不同：方法执行结束出栈时，局部变量销毁，但是局部内部类对局部变量的引用依然存在，当局部内部类需要使用该局部变量时，就会出现非法引用
+//                2.数据不同步：
+//                    内部类并不是直接使用局部变量，而是通过自己的构造备份到内部，表面上看是一个变量，实际上并不是
+//                    假如局部内部类对该变量做了修改，那么实际上该局部变量在局部内部类外的值不会改变，这就很尴尬，加上final就可以阻止这样的事发生
+//                而final加载进常量池，就能解决以上问题，对于jdk8，会默认加上final，不需要显式处理
+//    4.匿名内部类 内部类的简化写法
+//        1.格式    跳过命名，直接继承一个类或是实现一个接口（可以是具体类也可以是抽象类）
+//            new 类名或者接口名(){
+//                重写方法;
+//            }.方法名();
+//            后面可以直接跟.方法名调用对象的成员方法
+//            所以其实，本质上是子类（实现类）匿名对象
 
 public class BMianXiangDuiXiang {
+//    private String namePri = "Pri";
+//    String name = "";
+//    protected String namePro = "Pro";
+//    public String namePub = "Pub";
+
     public static void main(String[] args) {
 //        Father2B1 father2B1 = new Son2B1();
 //        father2B1.show();
 //
 //        Son2B2 son2B2 = new Son2B2();
 //        son2B2.show();
+
+//        Qualifier qua1 = new Qualifier();
+//        String name1 = qua1.name;       //default成员在同一包下可以访问
+//        String name2 = qua1.namePro;    //projected成员在同一包下，或者不同包下的子类中可以访问
+//        String name3 = qua1.namePub;    //public成员在不同包下仍可以访问
+//        String name4 = qua1.namePri;    //报错，因为private成员的除了在本类中，都不能访问（但可以提供公共方法去访问）
+
+//        OuterClass.InnerClass innerClass = new OuterClass().new InnerClass();
+//        OuterClass.StaticInnerClass staticInnerClass = new OuterClass.StaticInnerClass();
+//        innerClass.showOuterName();
+//        staticInnerClass.showOuterName();
     }
 }
 
@@ -115,3 +198,35 @@ public class BMianXiangDuiXiang {
 //        System.out.print(Father2B2.name + "\n"+ Father2B3.name);
 //    }
 //}
+
+//成员内部类
+class OuterClass {
+    private String name = "OuterName";
+    class InnerClass {
+        private String name = "InnerName";
+        public void showOuterName() {
+            String name = "name";
+            System.out.println(name);
+            System.out.println(this.name);
+            System.out.println(OuterClass.this.name);//通过外部类的引用直接访问外部类成员（包括私有的）
+        }
+    }
+    static class StaticInnerClass {
+        private String name = "InnerName";
+        public void showOuterName() {
+            String name = "name";
+            System.out.println(name);
+            System.out.println(this.name);
+        }
+    }
+    public void show() {
+        final int age = 10;
+        class inner {
+            public void showAge() {
+                System.out.println(age);
+            }
+        }
+    }
+}
+
+
