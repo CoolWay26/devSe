2381a6b032470d6c8ed717541fe08d35c33b4ec1
CoolWay26 <377905399@qq.com>
2020-02-06 16:47:33 +0800

    复习，提取知识点

--- src/learnSe/part2/BObjectOriented.java
+++ src/learnSe/part2/BObjectOriented.java
@@ -2,15 +2,23 @@ package learnSe.part2;
 //2.2面向对象（Ⅱ）
 //知识点
 //记忆
-//    1.抽象类（子类，abstract）
-//    2.接口（成员特点，和抽象类区别）
-//    3.权限修饰符（注意projected的说明）
-//    4.内部类（成员内部类，局部内部类，匿名局部内部类本质是子类对象-重写方法并调用）
+//    1.抽象类（特点，成员，abstract）
+//    2.接口（成员特点，抽象类与接口区别）
+//    3.权限修饰符（记清作用，注意projected的说明）
+//    4.package（import作用）
+//    5.内部类
+//            一般成员内部类：定义，如何使用
+//            私有内部类：通过外部类的public方法访问
+//            静态内部类：外部类名调用new内部类创建对象，new习惯提前
+//            局部内部类：属于方法的内部类，只能在方法内调用；如果要使用所在方法的局部变量，该变量必须加上final，JDK8取消了这个东西
+//            匿名局部内部类：用在方法中，本质是子类对象，重写父类或者接口的方法并调用
 //了解
+//
 //1.抽象类
 //    1.特点
 //        1.abstract修饰（抽象类和抽象方法）
 //        2.抽象类不一定要有抽象方法，但有抽象方法的一定是抽象类或者接口
+//            抽象方法：被abstract修饰，没有方法体的方法
 //        3.抽象类不能被实例化，但是可以通过多态的形式用子类实例化（就算抽象类直接实例化也没有什么意义）
 //        4.抽象类的子类要么是抽象类，要么就重写父类所有的抽象方法
 //    2.成员
@@ -50,8 +58,7 @@ package learnSe.part2;
 //        3.成员方法  必须抽象，   自带修饰符public abstract
 //            public：对实现类开放
 //            abstract：强制实现类重写
-//            JKD8以后可以有方法体（编写默认实现）
-//
+//            JKD8以后可以有方法体（编写默认实现），JDK8的新特性
 //            interface Father2B2 {
 //                String name = "Father2B2:interface!";
 //                abstract void show();
@@ -78,7 +85,7 @@ package learnSe.part2;
 //                构造方法	没有
 //                成员方法	只能是抽象方法 public abstract
 //        2.关系区别
-//            类与类	    继承，单继承，多层继承
+//            类与类	继承，单继承，多层继承
 //            类与接口	实现，单实现，多实现  class Demo implements InterA,InterB {}
 //            接口与接口	继承，单继承，多继承  interface Demo extends InterA,InterB {}
 //        3.设计理念区别
@@ -107,7 +114,7 @@ package learnSe.part2;
 //    projected:
 //        本包中，不同包的子类中通过super或者（this）访问，其实super和this访问的是同一个对象
 //        要明确的是，实际上并不是直接访问父类，而是访问继承而来的成员（其实还是访问子类对象）
-//        并不能通过父类对象直接访问projected成员
+//        并不能通过父类对象直接访问projected成员，可以理解成projected修饰的变量可以被子类继承并访问
 //            比如：子父类不在同一包下，
 //            package p1;
 //            class Fu {
@@ -131,21 +138,74 @@ package learnSe.part2;
 //    抽象修饰符：abstract
 //6.成员内部类（归根到底还是外部类的成员）
 //    1.特点
-//        1.内部类中可以直接访问外部类的成员，包括私有（因为内部类可以获得外部类的引用Outer.this）
-//        2.外部类要访问内部类的成员，必须创建对象：Outer.Inner oi = new Outer().new Inner();
+//        1.内部类中可以直接使用外部类的成员，包括私有（因为内部类可以获得外部类的引用Outer.this）
+//        2.外部类要使用内部类的成员，必须创建对象：Inner inner = new Inner()；外部类之外要访问则需要Outer.Inner inner = new Outer().new Inner()
 //        3.和其他的类一样，成员可以定义成private，提供public方法访问
 //        4.内部类本身访问权限不能超过外部类（亲测），这也很好理解，因为访问内部类的前提是访问外部类
+//    2.私有成员内部类   用private修饰
+//        如果内部类private,那么将无法在外部类外面访问它，只能通过外部类的public方法访问
 //    2.静态成员内部类   用static修饰
-//        外部类名.内部类名 对象名 = 外部类名.内部类对象; Outer.Inner oi = new Outer.Inner();相当于外部类名调用内部静态成员，习惯把new提前
+//        外部类名.内部类名 对象名 = 外部类名.内部类对象; 相当于外部类名调用内部静态成员，习惯把new提前
+//        Outer.Inner oi = new Outer.Inner();
+//        如果是访问静态内部类的静态成员，Outer.Inner.staticMethod();即可
 //    3.局部内部类（成员方法内的内部类）
-//        jdk8以前，局部内部类访问其所在方法的局部变量时，该变量必须是final修饰的自定义常量
+//        局部内部类只能在定义所在的方法中访问
+//        jdk8以前，局部内部类访问其所在方法的局部变量时，该变量必须是显式的final修饰的自定义常量
 //            原因：
 //                1.生命周期不同：方法执行结束出栈时，局部变量销毁，但是局部内部类对局部变量的引用依然存在，当局部内部类需要使用该局部变量时，就会出现非法引用
 //                2.数据不同步：
-//                    内部类并不是直接使用局部变量，而是通过自己的构造备份到内部，表面上看是一个变量，实际上并不是
+//                    局部内部类并不是直接使用局部变量，而是通过自己的构造备份到内部，表面上看是一个变量，实际上并不是
 //                    假如局部内部类对该变量做了修改，那么实际上该局部变量在局部内部类外的值不会改变，这就很尴尬，加上final就可以阻止这样的事发生
 //                而final加载进常量池，就能解决以上问题，对于jdk8，会默认加上final，不需要显式处理
-//    4.匿名局部内部类 内部类的简化写法
+//            class OuterClass {
+//                private String name = "OuterName";
+//
+//                //一般成员内部类
+//                class InnerClass {
+//                    private String name = "InnerName";
+//                    public void showOuterName() {
+//                        String name = "name";
+//                        System.out.println(name);//就近
+//                        System.out.println(this.name);//本类
+//                        System.out.println(OuterClass.this.name);//通过外部类的引用直接访问外部类成员（包括私有的）
+//                    }
+//                }
+//
+//                //私有成员内部类
+//                private class PriInnerClass {
+//                    private int priInnerAge;
+//                    public void getInAge() {
+//                        System.out.println("OuterName:" + name);//内部类可以直接访问外部类成员，本质上是因为可以获取Outer.this
+//                        System.out.println("PriInnerAge:" + priInnerAge);
+//                    }
+//                }
+//                //如果内部类private，可以通过public方法访问该内部类，在外部类之外无法直接通过Outer.Inner inner = new Outer().new Inner();访问内部类
+//                public void getPriInnerAge() {
+//                    PriInnerClass inner = new PriInnerClass();
+//                    inner.getInAge();
+//                }
+//
+//                //静态成员内部类，看作静态成员即可
+//                static class StaticInnerClass {
+//                    private String name = "InnerName";
+//                    public void showOuterName() {
+//                        String name = "name";
+//                        System.out.println(name);
+//                        System.out.println(this.name);
+//                    }
+//                }
+//
+//                //局部内部类
+//                public void show() {
+//                    final int age = 10;
+//                    class inner {
+//                        public void showAge() {
+//                            System.out.println(age);
+//                        }
+//                    }
+//                }
+//            }
+//    4.匿名局部内部类 局部内部类的简化写法
 //        1.格式    跳过命名，直接继承一个类或是实现一个接口（可以是具体类也可以是抽象类）
 //            new 类名或者接口名(){
 //                重写方法;
@@ -180,7 +240,36 @@ package learnSe.part2;
 //                String name = "InnerClass";
 //                void showName();
 //            }
-
+//        2.应用
+//            作为方法参数传递，方便在方法内直接调用重写后的方法，见下
+//                class Outer {
+//                    //内部类实现接口重写方法，这样可以通过该内部类调用重写的方法
+//                    class Inner implements ForInner {
+//                        @Override
+//                        public void showName() {
+//                            System.out.println(name);
+//                        }
+//                    }
+//
+//                匿名局部内部类直接重写和调用
+//                    public void showInner() {
+//                        new ForInner() {
+//                            @Override
+//                            public void showName() {
+//                                System.out.println(name);
+//                            }
+//                        }.showName();
+//                    }
+//
+//                调用toShow()方法时，匿名内部类作为参数传递（即在调用toShow()方法时，传入一个匿名局部内部类对showName()进行重写即可）
+//                    public void toShow(ForInner forInner) {
+//                        forInner.showName();
+//                    }
+//                }
+//                interface ForInner {
+//                    String name = "InnerClass";
+//                    void showName();
+//                }
 public class BObjectOriented {
 //    private String namePri = "Pri";
 //    String name = "";
@@ -222,84 +311,7 @@ public class BObjectOriented {
 //    }
 //}
 
-//接口
-//interface Father2B2 {
-//    String name = "Father2B2:interface!";
-//    abstract void show();
-//}
-//interface Father2B3 {
-//    String name = "Father2B3:interface!";
-//    abstract void show();
-//}
-//
-//class Son2B2 implements Father2B2,Father2B3 {
-//
-//    @Override
-//    public void show() {
-//        System.out.print(Father2B2.name + "\n"+ Father2B3.name);
-//    }
-//}
 
-//成员内部类
-//class OuterClass {
-//    private String name = "OuterName";
-//    class InnerClass {
-//        private String name = "InnerName";
-//        public void showOuterName() {
-//            String name = "name";
-//            System.out.println(name);
-//            System.out.println(this.name);
-//            System.out.println(OuterClass.this.name);//通过外部类的引用直接访问外部类成员（包括私有的）
-//        }
-//    }
-//    static class StaticInnerClass {
-//        private String name = "InnerName";
-//        public void showOuterName() {
-//            String name = "name";
-//            System.out.println(name);
-//            System.out.println(this.name);
-//        }
-//    }
-//    public void show() {
-//        final int age = 10;
-//        class inner {
-//            public void showAge() {
-//                System.out.println(age);
-//            }
-//        }
-//    }
-//}
-
-//匿名局部内部类
-//class Outer {
-//    //内部类实现接口重写方法，这样可以通过该内部类调用重写的方法
-//    class Inner implements ForInner {
-//        @Override
-//        public void showName() {
-//            System.out.println(name);
-//        }
-//    }
-//
-//    public void showInner() {
-//        //匿名局部内部类直接重写和调用
-//        new ForInner() {
-//            @Override
-//            public void showName() {
-//                System.out.println(name);
-//            }
-//        }.showName();
-//    }
-//
-//    //调用toShow()方法时，匿名内部类作为参数传递（重写showName()方法），也可以实现功能
-//    public void toShow(ForInner forInner) {
-//        forInner.showName();
-//    }
-//}
-//
-//interface ForInner {
-//    String name = "InnerClass";
-//    void showName();
-//}
 
 
 
